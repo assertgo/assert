25ce4d3c20960eac32daf23eab12642ee29732dc
Maciej Górski <maciek.gorski@gmail.com>
2014-12-13 00:51:34 +0100

    test for IsZero message on fail

--- that_int.go
+++ that_int.go
@@ -1,9 +1,18 @@
 package assert
 
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+var writer io.Writer = os.Stdout
+
 type ThatInt int
 
 func (thatInt ThatInt) IsZero() {
 	if thatInt != 0 {
+		fmt.Fprintln(writer, "Expected <0>, but was <1>.")
 		panic("that int should be zero")
 	}
 }
--- that_int_test.go
+++ that_int_test.go
@@ -1,6 +1,9 @@
 package assert
 
-import "testing"
+import (
+	"bytes"
+	"testing"
+)
 
 func TestThatIntIsZero(t *testing.T) {
 	ThatInt(0).IsZero()
@@ -10,6 +13,20 @@ func TestThatIntIsZeroFails(t *testing.T) {
 	defer _recover()
 	ThatInt(1).IsZero()
 	t.Fail()
+}
+
+func TestThatIntIsZeroPrintsMessage(t *testing.T) {
+	b := &bytes.Buffer{}
+	prod := writer
+	writer = b
+	defer func() {
+		recover()
+		if b.String() != "Expected <0>, but was <1>.\n" {
+			t.Fail()
+		}
+		writer = prod
+	}()
+	ThatInt(1).IsZero()
 }
 
 func _recover() {
