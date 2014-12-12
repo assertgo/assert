5a92a0f7a3731e2c800ecb9807da964e58c9ce70
Maciej Górski <maciek.gorski@gmail.com>
2014-12-13 00:06:00 +0100

    test ThatInt IsZero fails for non zero args

--- that_int.go
+++ that_int.go
@@ -3,5 +3,7 @@ package assert
 type ThatInt int
 
 func (thatInt ThatInt) IsZero() {
-
+	if thatInt != 0 {
+		panic("that int should be zero")
+	}
 }
--- that_int_test.go
+++ that_int_test.go
@@ -5,3 +5,13 @@ import "testing"
 func TestThatIntIsZero(t *testing.T) {
 	ThatInt(0).IsZero()
 }
+
+func TestThatIntIsZeroFails(t *testing.T) {
+	defer _recover()
+	ThatInt(1).IsZero()
+	t.Fail()
+}
+
+func _recover() {
+	recover()
+}
