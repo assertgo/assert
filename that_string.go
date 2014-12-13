ac87c5bab1b212c1558f6b742d34b754b689c23c
Maciej Górski <maciek.gorski@gmail.com>
2014-12-13 17:18:44 +0100

    chaining ThatString methods

--- that_string.go
+++ that_string.go
@@ -4,9 +4,10 @@ import "fmt"
 
 type ThatString string
 
-func (actual ThatString) IsEqualTo(expected string) {
+func (actual ThatString) IsEqualTo(expected string) ThatString {
 	if string(actual) != expected {
 		message := fmt.Sprintf("Expected <%s>, but was <%s>.\n", expected, actual)
 		fmt.Fprint(writer, message)
 	}
+	return actual
 }
