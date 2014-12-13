7dd82cbe66c43aab10ff2dfb8b3cf5909d60acaa
Michał Warda <michal.warda@elpassion.pl>
2014-12-13 12:32:17 +0100

    Add ThatString IsEqualTo method and test it using previous test basics.

--- that_string.go
+++ that_string.go
@@ -1,9 +1,12 @@
 package assert
 
+import "fmt"
+
 type ThatString string
 
 func (thatString ThatString) IsEqualTo(comparedString string) {
 	if string(thatString) != comparedString {
+		fmt.Fprintf(writer, "Expected <%s>, but was <%s>.\n", thatString, comparedString)
 		panic("")
 	}
 }
--- that_string_test.go
+++ that_string_test.go
@@ -1,17 +1,30 @@
 package assert
 
-import "testing"
+import (
+	"bytes"
+	"io/ioutil"
+	"testing"
+)
 
 func TestThatStringIsEqualTo(t *testing.T) {
 	ThatString("Alice has a cat.").IsEqualTo("Alice has a cat.")
 }
 
 func TestThatStringIsEqualToFails(t *testing.T) {
-	defer _recover()
+	recoverAndRestore := mockWriter(ioutil.Discard)
+	defer recoverAndRestore()
 	ThatString("Alice has a cat.").IsEqualTo("Bob has a dog.")
 	t.Fail()
 }
 
-func _recover() {
-	recover()
+func TestThatStringIsEqualToPrintsMessage(t *testing.T) {
+	buffer := &bytes.Buffer{}
+	assert := func() {
+		if buffer.String() != "Expected <Alice has a cat.>, but was <Bob has a dog.>.\n" {
+			t.Error("Expected <Expected <Alice has a cat.>, but was <Bob has a dog.>\n.>, but was <" + buffer.String() + ">.")
+		}
+	}
+	recoverAndRestore := mockWriter(buffer, assert)
+	defer recoverAndRestore()
+	ThatString("Alice has a cat.").IsEqualTo("Bob has a dog.")
 }
