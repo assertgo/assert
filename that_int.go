a9ddc4d6ff4427bfaf6a0df1db8578063d9d6441
Maciej Górski <maciek.gorski@gmail.com>
2014-12-14 19:03:12 +0100

    refactoring: extracted IntAssert interface

--- that_int.go
+++ that_int.go
@@ -5,103 +5,122 @@ import (
 	"strings"
 )
 
-func ThatInt(t TestingT, actual int) *IntAssert {
-	return &IntAssert{t, actual}
+func ThatInt(t TestingT, actual int) IntAssert {
+	return &intAssertImpl{t, actual}
 }
 
-type IntAssert struct {
+type IntAssert interface {
+	IsZero() IntAssert
+	IsNonzero() IntAssert
+	IsEqualTo(int) IntAssert
+	IsNotEqualTo(int) IntAssert
+	IsPositive() IntAssert
+	IsNonpositive() IntAssert
+	IsNegative() IntAssert
+	IsNonnegative() IntAssert
+	IsGreaterThan(int) IntAssert
+	IsGreaterOrEqualTo(int) IntAssert
+	IsLessThan(int) IntAssert
+	IsLessOrEqualTo(int) IntAssert
+	IsBetween(int, int) IntAssert
+	IsNotBetween(int, int) IntAssert
+	IsIn(...int) IntAssert
+	IsNotIn(...int) IntAssert
+}
+
+type intAssertImpl struct {
 	t      TestingT
 	actual int
 }
 
-func (assert *IntAssert) isTrue(condition bool, format string, args ...interface{}) *IntAssert {
+func (assert *intAssertImpl) isTrue(condition bool, format string, args ...interface{}) *intAssertImpl {
 	if !condition {
 		assert.t.Errorf(format, args...)
 	}
 	return assert
 }
 
-func (assert *IntAssert) IsZero() *IntAssert {
+func (assert *intAssertImpl) IsZero() IntAssert {
 	return assert.isTrue(assert.actual == 0,
 		"Expected zero, but was <%d>.", assert.actual)
 }
 
-func (assert *IntAssert) IsNonzero() *IntAssert {
+func (assert *intAssertImpl) IsNonzero() IntAssert {
 	return assert.isTrue(assert.actual != 0,
 		"Expected nonzero, but was zero.")
 }
 
-func (assert *IntAssert) IsEqualTo(expected int) *IntAssert {
+func (assert *intAssertImpl) IsEqualTo(expected int) IntAssert {
 	return assert.isTrue(assert.actual == expected,
 		"Expected integer equal to <%d>, but was <%d>.", expected, assert.actual)
 }
 
-func (assert *IntAssert) IsNotEqualTo(nonexpected int) *IntAssert {
+func (assert *intAssertImpl) IsNotEqualTo(nonexpected int) IntAssert {
 	return assert.isTrue(assert.actual != nonexpected,
 		"Expected integer not equal to <%d>, but was equal.", nonexpected)
 }
 
-func (assert *IntAssert) IsPositive() *IntAssert {
+func (assert *intAssertImpl) IsPositive() IntAssert {
 	return assert.isTrue(assert.actual > 0,
 		"Expected positive integer, but was <%d>.", assert.actual)
 }
 
-func (assert *IntAssert) IsNonpositive() *IntAssert {
+func (assert *intAssertImpl) IsNonpositive() IntAssert {
 	return assert.isTrue(assert.actual <= 0,
 		"Expected nonpositive integer, but was <%d>.", assert.actual)
 }
 
-func (assert *IntAssert) IsNegative() *IntAssert {
+func (assert *intAssertImpl) IsNegative() IntAssert {
 	return assert.isTrue(assert.actual < 0,
 		"Expected negative integer, but was <%d>.", assert.actual)
 }
 
-func (assert *IntAssert) IsNonnegative() *IntAssert {
+func (assert *intAssertImpl) IsNonnegative() IntAssert {
 	return assert.isTrue(assert.actual >= 0,
 		"Expected nonnegative integer, but was <%d>.", assert.actual)
 }
 
-func (assert *IntAssert) IsGreaterThan(lesser int) *IntAssert {
+func (assert *intAssertImpl) IsGreaterThan(lesser int) IntAssert {
 	return assert.isTrue(assert.actual > lesser,
 		"Expected integer greater than <%d>, but was <%d>.", lesser, assert.actual)
 }
 
-func (assert *IntAssert) IsGreaterOrEqualTo(lesserOrEqual int) *IntAssert {
+func (assert *intAssertImpl) IsGreaterOrEqualTo(lesserOrEqual int) IntAssert {
 	return assert.isTrue(assert.actual >= lesserOrEqual,
 		"Expected integer greater or equal to <%d>, but was <%d>.", lesserOrEqual, assert.actual)
 }
 
-func (assert *IntAssert) IsLessThan(greater int) *IntAssert {
+func (assert *intAssertImpl) IsLessThan(greater int) IntAssert {
 	return assert.isTrue(assert.actual < greater,
 		"Expected integer lesser than <%d>, but was <%d>.", greater, assert.actual)
 }
 
-func (assert *IntAssert) IsLessOrEqualTo(greaterOrEqual int) *IntAssert {
+func (assert *intAssertImpl) IsLessOrEqualTo(greaterOrEqual int) IntAssert {
 	return assert.isTrue(assert.actual <= greaterOrEqual,
 		"Expected integer lesser or equal to <%d>, but was <%d>.", greaterOrEqual, assert.actual)
 }
 
-func (assert *IntAssert) IsBetween(lesserOrEqual, greaterOrEqual int) *IntAssert {
+func (assert *intAssertImpl) IsBetween(lesserOrEqual, greaterOrEqual int) IntAssert {
 	return assert.isTrue(lesserOrEqual <= assert.actual && assert.actual <= greaterOrEqual,
 		"Expected integer to be between <%d, %d>, but was <%d>.", lesserOrEqual, greaterOrEqual, assert.actual)
 }
 
-func (assert *IntAssert) IsNotBetween(nonlesser, nongreater int) *IntAssert {
+func (assert *intAssertImpl) IsNotBetween(nonlesser, nongreater int) IntAssert {
 	return assert.isTrue(assert.actual < nonlesser || assert.actual > nongreater,
 		"Expected integer not to be between <%d, %d>, but was <%d>.", nonlesser, nongreater, assert.actual)
 }
 
-func (assert *IntAssert) IsIn(expected ...int) *IntAssert {
+func (assert *intAssertImpl) IsIn(expected ...int) IntAssert {
 	return assert.isTrue(assert.isIn(expected...),
 		"Expected integer to be in (%s), but was <%d>.", strings.Join(strSlice(expected...), ", "), assert.actual)
 }
 
-func (assert *IntAssert) IsNotIn(expected ...int) *IntAssert {
+func (assert *intAssertImpl) IsNotIn(expected ...int) IntAssert {
 	return assert.isTrue(!assert.isIn(expected...),
 		"Expected integer not to be in (%s), but was <%d>.", strings.Join(strSlice(expected...), ", "), assert.actual)
 }
 
-func (assert *IntAssert) isIn(expected ...int) bool {
+func (assert *intAssertImpl) isIn(expected ...int) bool {
 	for _, v := range expected {
 		if assert.actual == v {
 			return true
