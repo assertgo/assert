24884e36e0242e3ce2901945009788009e9e037f
Maciej Górski <maciek.gorski@gmail.com>
2014-12-14 23:46:26 +0100

    refactoring: moved assertProviderImpl to a new file

---
+++ assert_impl.go
@@ -0,0 +1,13 @@
+package assert
+
+type assertProviderImpl struct {
+	t TestingT
+}
+
+func (assertProvider *assertProviderImpl) ThatInt(actual int) IntAssert {
+	return ThatInt(assertProvider.t, actual)
+}
+
+func (assertProvider *assertProviderImpl) ThatString(actual string) StringAssert {
+	return ThatString(assertProvider.t, actual)
+}
