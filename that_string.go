2c4f6d859e745a16685beec1f1cb0934d06eb596
Michał Warda <michal.warda@elpassion.pl>
2014-12-14 19:43:43 +0100

    Change StringAssert struct name.

--- that_string.go
+++ that_string.go
@@ -1,22 +1,22 @@
 package assert
 
-func ThatString(t TestingT, actual string) *StringAssert {
-	return &StringAssert{t, actual}
+func ThatString(t TestingT, actual string) *StringAssertImpl {
+	return &StringAssertImpl{t, actual}
 }
 
-type StringAssert struct {
+type StringAssertImpl struct {
 	t      TestingT
 	actual string
 }
 
-func (assert *StringAssert) isTrue(condition bool, format string, args ...interface{}) *StringAssert {
+func (assert *StringAssertImpl) isTrue(condition bool, format string, args ...interface{}) *StringAssertImpl {
 	if !condition {
 		assert.t.Errorf(format, args...)
 	}
 	return assert
 }
 
-func (assert *StringAssert) IsEqualTo(expected string) *StringAssert {
+func (assert *StringAssertImpl) IsEqualTo(expected string) *StringAssertImpl {
 	return assert.isTrue(assert.actual == expected,
 		"Expected <%s>, but was <%s>.", expected, assert.actual)
 }
