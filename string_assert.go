a76754b0a56f2e4247a9c4629811c3cb2e79eb4d
Maciej Górski <maciek.gorski@gmail.com>
2014-12-17 15:21:42 +0100

    a few ideas for StringAssert

--- string_assert.go
+++ string_assert.go
@@ -12,4 +12,15 @@ type StringAssert interface {
 	Contains(substring string) StringAssert
 	// TODO
 	// Matches(pattern string) StringAssert
+	// IsNotEmpty() StringAssert
+	// DoesNotContain(substring string) StringAssert
+	// IsNotInSlice(elements []string) StringAssert
+	// IsIn(elements ...string) StringAssert
+	// IsNotIn(elements ...string) StringAssert
+	// HasPrefix(prefix string) StringAssert
+	// HasSuffix(suffix string) StringAssert
+	// IsLowerCase() StringAssert
+	// IsUpperCase() StringAssert
+	// IgnoringCase() StringAssert // new assert that doesn't care about case
+	// Length() IntAssert // new assert to check on length of the string
 }
