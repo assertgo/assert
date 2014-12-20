b75f16eed59a28dce069cb7c736a5ffa9d6480c2
Maciej Górski <maciek.gorski@gmail.com>
2014-12-20 16:20:49 +0100

    refactoring: moved errorLogger to a new file

--- common_impl.go
+++ common_impl.go
@@ -1,31 +1,5 @@
 package assert
 
-import (
-	"io"
-	"os"
-)
-
-type errorLogger interface {
-	Log(location *location, message string)
-}
-
-func newLogger() errorLogger {
-	return &errorLoggerImpl{os.Stderr}
-}
-
-type errorLoggerImpl struct {
-	writer io.Writer
-}
-
-func (logger *errorLoggerImpl) Log(location *location, message string) {
-}
-
-type location struct {
-	Test     string
-	FileName string
-	Line     int
-}
-
 func logIfFalse(t TestingT, condition bool, format string, args ...interface{}) {
 	if !condition {
 		t.Errorf(format, args...)
---
+++ error_logger_impl.go
@@ -0,0 +1,27 @@
+package assert
+
+import (
+	"io"
+	"os"
+)
+
+type location struct {
+	Test     string
+	FileName string
+	Line     int
+}
+
+type errorLogger interface {
+	Log(location *location, message string)
+}
+
+func newLogger() errorLogger {
+	return &errorLoggerImpl{os.Stderr}
+}
+
+type errorLoggerImpl struct {
+	writer io.Writer
+}
+
+func (logger *errorLoggerImpl) Log(location *location, message string) {
+}
