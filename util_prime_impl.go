5b4b127e24987c90a6383f995ba5c38dcc4f5247
Maciej Górski <maciek.gorski@gmail.com>
2014-12-20 06:36:33 +0100

    fixed infinite loop on 32-bit machines (e.g. playground server)

--- util_prime_impl.go
+++ util_prime_impl.go
@@ -7,8 +7,9 @@ func isPrime(value int) bool {
 	if value%2 == 0 || value%3 == 0 {
 		return false
 	}
-	for i := 5; i*i <= value; i += 6 {
-		if value%i == 0 || value%(i+2) == 0 {
+	v64 := int64(value)
+	for i := int64(5); i*i <= v64; i += 6 {
+		if v64%i == 0 || v64%(i+2) == 0 {
 			return false
 		}
 	}
