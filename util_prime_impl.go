d7453d191779b61d2663644bb03ed999b8e66a3b
Maciej Górski <maciek.gorski@gmail.com>
2014-12-20 15:19:10 +0100

    isPrime will work with primes close to 2^63-1 on 64-bit machines

--- util_prime_impl.go
+++ util_prime_impl.go
@@ -7,9 +7,9 @@ func isPrime(value int) bool {
 	if value%2 == 0 || value%3 == 0 {
 		return false
 	}
-	v64 := int64(value)
-	for i := int64(5); i*i <= v64; i += 6 {
-		if v64%i == 0 || v64%(i+2) == 0 {
+	uns := uint(value)
+	for i := uint(5); i*i <= uns; i += 6 {
+		if uns%i == 0 || uns%(i+2) == 0 {
 			return false
 		}
 	}
