facdd0ff5409b35a2c0c00a384799e6bf3591491
Maciej Górski <maciek.gorski@gmail.com>
2014-12-20 14:37:05 +0100

    refactoring: changed test method names

--- util_prime_test.go
+++ util_prime_test.go
@@ -28,26 +28,26 @@ func TestSmallNumbersAreNotPrime(t *testing.T) {
 	assert.ThatBool(isPrime(9)).IsFalse()
 }
 
-func TestThatBigNumbersArePrime(t *testing.T) {
+func TestBigNumbersArePrime(t *testing.T) {
 	assert := Setup(t)
 	assert.ThatBool(isPrime(2311)).IsTrue()
 	assert.ThatBool(isPrime(3203)).IsTrue()
 	assert.ThatBool(isPrime(1321)).IsTrue()
 }
 
-func TestThatBigNumbersAreNotPrime(t *testing.T) {
+func TestBigNumbersAreNotPrime(t *testing.T) {
 	assert := Setup(t)
 	assert.ThatBool(isPrime(1001)).IsFalse()
 	assert.ThatBool(isPrime(5251)).IsFalse()
 	assert.ThatBool(isPrime(2809)).IsFalse()
 }
 
-func TestThatReallyBigNumberIsPrime(t *testing.T) {
+func TestReallyBigNumberIsPrime(t *testing.T) {
 	assert := Setup(t)
 	assert.ThatBool(isPrime(2147483647)).IsTrue()
 }
 
-func TestThatReallyBigNumberIsNotPrime(t *testing.T) {
+func TestReallyBigNumberIsNotPrime(t *testing.T) {
 	assert := Setup(t)
 	assert.ThatBool(isPrime(46337 * 46337)).IsFalse()
 }
