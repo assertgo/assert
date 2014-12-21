d965b6a56a4299f9b1d70880ef889805b0873581
Maciej Górski <maciek.gorski@gmail.com>
2014-12-21 22:38:39 +0100

    implementation of That.IsEqualTo

--- any_type_assert_impl.go
+++ any_type_assert_impl.go
@@ -1,10 +1,20 @@
 package assert
 
+import (
+	"reflect"
+)
+
 type anyTypeAssertImpl struct {
 	logFacade *logFacade
 	actual    interface{}
 }
 
-func (assert *anyTypeAssertImpl) IsEqualTo(expected interface{}) AnyTypeAssert {
+func (assert *anyTypeAssertImpl) isTrue(condition bool, format string, args ...interface{}) *anyTypeAssertImpl {
+	logIfFalse(assert.logFacade, condition, format, args...)
 	return assert
+}
+
+func (assert *anyTypeAssertImpl) IsEqualTo(expected interface{}) AnyTypeAssert {
+	return assert.isTrue(reflect.DeepEqual(assert.actual, expected),
+		"Expected <%v>, but was <%v>.", expected, assert.actual)
 }
--- any_type_assert_test.go
+++ any_type_assert_test.go
@@ -4,8 +4,18 @@ import "testing"
 
 func TestThatIsEqualToHasNoErrors(t *testing.T) {
 	assert, mockT := setupWithMockT(t)
-	assert.That(TestThatIsEqualToHasNoErrors).
-		IsEqualTo(TestThatIsEqualToHasNoErrors).
-		IsEqualTo(TestThatIsEqualToHasNoErrors)
+	assert.That(&location{"TestFoo", "foo_test.go", 66}).
+		IsEqualTo(&location{"TestFoo", "foo_test.go", 66}).
+		IsEqualTo(&location{"TestFoo", "foo_test.go", 66})
 	mockT.HasNoErrors()
+}
+
+func TestThatIsEqualToHasErrorMessages(t *testing.T) {
+	assert, mockT := setupWithMockT(t)
+	assert.That(location{"TestFoo", "foo_test.go", 66}).IsEqualTo(location{"TestBar", "bar_test.go", 66})
+	assert.That(&location{"TestFoo", "foo_test.go", 66}).IsEqualTo(&location{"TestFoo", "foo_test.go", 33})
+	mockT.HasErrorMessages(
+		"Expected <{TestBar bar_test.go 66}>, but was <{TestFoo foo_test.go 66}>.",
+		"Expected <&{TestFoo foo_test.go 33}>, but was <&{TestFoo foo_test.go 66}>.",
+	)
 }
