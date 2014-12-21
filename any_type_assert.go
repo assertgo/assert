fc2c40b7d9609154c2333db65daa4e5c9eb9e3d6
Maciej Górski <maciek.gorski@gmail.com>
2014-12-21 23:01:17 +0100

    some ideas for any type assertions

--- any_type_assert.go
+++ any_type_assert.go
@@ -2,4 +2,10 @@ package assert
 
 type AnyTypeAssert interface {
 	IsEqualTo(expected interface{}) AnyTypeAssert
+	// TODO
+	// IsNotEqualTo(unexpected interface{}) AnyTypeAssert
+	// IsNil() AnyTypeAssert
+	// IsNotNil() AnyTypeAssert
+	// AsInt() IntAssert // new assert that treats actual value as int, reports failure if cannot convert
+	// AsString() StringAssert
 }
