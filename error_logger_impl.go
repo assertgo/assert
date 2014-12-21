4e2236b084b5297465eaaf9d14d6926d230025bd
Michał Warda <michal.warda@elpassion.pl>
2014-12-21 16:46:40 +0100

    Refactor Error logger test and implementation.

--- error_logger_impl.go
+++ error_logger_impl.go
@@ -23,11 +23,17 @@ type errorLoggerImpl struct {
 	alreadyWritten bool
 }
 
+const (
+	failOutput                = "--- FAIL: %s\n\t%s:%d\n\t\t%s\n"
+	failOutputWithoutFailLine = "\t%s:%d\n\t\t%s\n"
+)
+
 func (logger *errorLoggerImpl) Log(location *location, message string) {
+	args := []interface{}{location.Test, location.FileName, location.Line, message}
 	if logger.alreadyWritten == false {
-		fmt.Fprintf(logger.writer, "--- FAIL: %s\n\t%s:%d\n\t\t%s\n", location.Test, location.FileName, location.Line, message)
+		fmt.Fprintf(logger.writer, failOutput, args...)
 		logger.alreadyWritten = true
 	} else {
-		fmt.Fprintf(logger.writer, "\t%s:%d\n\t\t%s\n", location.FileName, location.Line, message)
+		fmt.Fprintf(logger.writer, failOutputWithoutFailLine, args[1:]...)
 	}
 }
--- error_logger_test.go
+++ error_logger_test.go
@@ -7,9 +7,7 @@ import (
 
 func TestErrorLoggerSimpleCase(t *testing.T) {
 	assert, buffer, logger := setupWithLogger(t)
-	location := &location{"TestSimple", "simple_test.go", 66}
-	message := "Expected KABOOM!"
-	logger.Log(location, message)
+	logger.Log(&location{"TestSimple", "simple_test.go", 66}, "Expected KABOOM!")
 	assert.ThatString(buffer.String()).IsEqualTo(
 		"--- FAIL: TestSimple\n" +
 			"\tsimple_test.go:66\n" +
@@ -18,12 +16,8 @@ func TestErrorLoggerSimpleCase(t *testing.T) {
 
 func TestErrorLoggerSecondError(t *testing.T) {
 	assert, buffer, logger := setupWithLogger(t)
-	locationFirst := &location{"TestSimple", "simple_test.go", 66}
-	messageFirst := "Expected KABOOM!"
-	locationSecond := &location{"TestSimple", "simple_test.go", 67}
-	messageSecond := "Expected BAZOOKA?"
-	logger.Log(locationFirst, messageFirst)
-	logger.Log(locationSecond, messageSecond)
+	logger.Log(&location{"TestSimple", "simple_test.go", 66}, "Expected KABOOM!")
+	logger.Log(&location{"TestSimple", "simple_test.go", 67}, "Expected BAZOOKA?")
 	assert.ThatString(buffer.String()).IsEqualTo(
 		"--- FAIL: TestSimple\n" +
 			"\tsimple_test.go:66\n" +
