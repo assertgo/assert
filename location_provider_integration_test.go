cf969aac2749ccdd2f3b8bcf58a8ed8016800661
Maciej Górski <maciek.gorski@gmail.com>
2014-12-22 00:08:34 +0100

    removed mockT as we don't really care about it

--- location_provider_integration_test.go
+++ location_provider_integration_test.go
@@ -7,13 +7,12 @@ import (
 
 func TestLocationProviderViaFramework(t *testing.T) {
 	assert := Setup(t)
-	mockT := &mockTestingT{t: t}
 	buffer := &bytes.Buffer{}
 	logger := &errorLoggerImpl{writer: buffer}
-	mockAssert := setupImpl(mockT, logger)
+	mockAssert := setupImpl(&testing.T{}, logger)
 	mockAssert.ThatBool(false).IsTrue()
 	assert.ThatString(buffer.String()).IsEqualTo(
 		"\n--- FAIL: TestLocationProviderViaFramework\n" +
-			"\tlocation_provider_integration_test.go:14\n" +
+			"\tlocation_provider_integration_test.go:13\n" +
 			"\t\tExpected <true>, but was <false>.\n")
 }
