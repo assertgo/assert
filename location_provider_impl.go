ce0239092c21d7877550b5ecd62d5e15a6f9e381
Michał Warda <michal.warda@elpassion.pl>
2014-12-21 18:49:49 +0100

    Extract parseTestName from provide location.

--- location_provider_impl.go
+++ location_provider_impl.go
@@ -7,11 +7,16 @@ import (
 
 func provideLocation() location {
 	pc, _, _, _ := runtime.Caller(1)
-	testName := runtime.FuncForPC(pc).Name()
-	index := strings.LastIndex(testName, ".")
-	testName = testName[index+1:]
+	testName := parseTestName(pc)
 	return location{
 		Test:     testName,
 		FileName: "location_provider_test.go",
 	}
+}
+
+func parseTestName(pc uintptr) (testName string) {
+	testName = runtime.FuncForPC(pc).Name()
+	index := strings.LastIndex(testName, ".")
+	testName = testName[index+1:]
+	return
 }
